cc4b43952f7937a5d99b6b1fe36ac0b7896886af
Pedro Henrique <pedro_henrique.rss@hotmail.com>
2026-07-31 22:49:59 -0300

    first commit

---
+++ src/test/java/br/com/leje/lancecerto/LanceCertoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leje.lancecerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanceCertoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
