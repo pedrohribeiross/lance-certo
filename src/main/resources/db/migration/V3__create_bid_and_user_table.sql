3aa5795e901268077c16aea118696164af95d767
Pedro Henrique <pedro_henrique.rss@hotmail.com>
2026-08-12 17:48:14 -0300

    style(database): format SQL table creation for users and bids

--- src/main/resources/db/migration/V3__create_bid_and_user_table.sql
+++ src/main/resources/db/migration/V3__create_bid_and_user_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users
 (
-    id    UUID PRIMARY KEY    NOT NULL,
-    name  VARCHAR(150)        NOT NULL,
+    id UUID PRIMARY KEY NOT NULL,
+    name VARCHAR(150) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
-    role  VARCHAR(20)         NOT NULL,
+    role VARCHAR(20) NOT NULL,
 
     CONSTRAINT chk_role_user CHECK ( role IN ('ADMIN', 'BIDDER') )
 );
@@ -16,10 +16,10 @@ VALUES (gen_random_uuid(), 'Breno Guilherme Cauê Araújo', 'breno-araujo80@emai
 
 CREATE TABLE bids
 (
-    id         UUID PRIMARY KEY         NOT NULL,
-    lot_id     UUID                     NOT NULL,
-    user_id    UUID                     NOT NULL,
-    value      NUMERIC(15, 2)           NOT NULL,
+    id UUID PRIMARY KEY NOT NULL,
+    lot_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    value NUMERIC(15,2) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
 
     CONSTRAINT fk_bd_lot FOREIGN KEY (lot_id) REFERENCES lots (id),
